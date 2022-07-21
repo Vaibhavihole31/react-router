072e0c0e9fc122d19954144c33557be413f48255
Vaibhavihole31 <itspinki05@gmail.com>
2022-07-21 16:27:39 +0530

    Inital commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,120 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+  useParams,
+  NavLink,
+  useNavigate,
+  useLocation
+} from 'react-router-dom'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/myapps" element={<Navigate replace to ="/Learn"/>}/>
+      <Route path="/learn" element={<Learn/>}>
+         <Route path="courses" element={<Courses/>}>
+           <Route path=":courseid" element={<CourseId/>}/>
+         </Route>
+         <Route path="bundles" element={<Bundles/>}/> 
+      </Route>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function Home (){
+  return(
+    <div> 
+      <h1> Home Routes </h1>
+      </div>
+  )
+}
+
+function Learn (){
+  return(
+    <div> 
+      <h1> Learn  </h1>
+      <h4> All courses are listed here </h4>
+      <Link className='btn btn-success' to="/learn/courses"> 
+      courses
+      </Link>
+      <Link className='btn btn-primary' to="/learn/bundles"> 
+      bundle
+      </Link>
+
+      <Outlet/>
+      </div>
+  );
+}
+
+function Courses (){
+  const courseList = ["React","Angular","Vue","Nodejs"]
+  const randomCourseName = courseList[Math.floor(Math.random()* courseList.length)]
+  return(
+    <div> 
+      <h1> Courses list </h1>
+      <h4> Courses card</h4>
+
+     <p> More test</p>
+     <NavLink 
+     style={({isActive})=>{
+      return{
+        backgroundColor : isActive ? "yellow" : "aqua",
+      }
+     }}
+     to={`/learn/courses/${randomCourseName}`}>
+      {randomCourseName} 
+      </NavLink>
+
+      <NavLink className="btn btn-light" to={`/learn/courses/tests`}>
+       tests
+      </NavLink>
+
+      <Outlet/>
+      </div>
+  );
+}
+
+function Bundles (){
+  return(
+    <div> 
+      <h1> Bundle list </h1>
+      <h4> Bundle card</h4>
+      </div>
+  );
+}
+
+function CourseId (){
+  const navigate = useNavigate()
+  const {courseid} = useParams();
+  return(
+    <div> 
+      <h1> URL params is :{courseid} </h1>
+      <button 
+      onClick={()=>{
+        navigate("/dashboard",{state : "499"});
+      }}
+      className='btn btn-warning'>price</button>
+      </div>
+  );
+}
+
+function Dashboard (){
+  const location = useLocation();
+  return(
+    <div> 
+      <h1> Info that is got here is {location.state}</h1>
+      </div>
+  );
+}
+
+
 reportWebVitals();
